82748bf528c02b4fdf0b78b62f95cb0fe42eb71f
gongyejian <gongyejian@qq.com>
2022-09-06 14:36:17 +0800

    first

---
+++ spa/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div>
+    <h2>mp4 demo</h2>
+    <video id="video1" width="420" controls>
+    <source src="./assets/video/mov_bbb.mp4" type="video/mp4">
+    您的浏览器不支持 HTML5 video 标签。
+  </video>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
